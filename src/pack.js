4a117f26d8936b9e9800ce56d549298fed52142a
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-05-04 11:36:32 -0300

    Add files via upload

---
+++ src/pack.js
@@ -0,0 +1,40 @@
+const pack = (date, user, wame) => {
+return `╔╦══• •✠•𝐇𝐢𝐧𝐚𝐭𝐚•✠ • •══╦╗
+║
+║ 𝐁𝐎𝐓 : 「 𝐇𝐢𝐧𝐚𝐭𝐚 」
+║
+║ 𝐏𝐑𝐄𝐅𝐈𝐗𝐎 : /
+║
+║ 𝐃𝐎𝐍𝐎 : wa.me/+5575998868032
+║
+║𝘿𝘼𝙏𝘼 𝙀 𝙃𝙊𝙍𝘼 : ${date} 
+║
+║𝙐𝙎𝙐𝘼𝙍𝙄𝙊 : ${user} 
+║
+║𝙒𝘼𝙈𝙀 : ${wame} 
+║
+╚╩══• •✠•𝐇𝐢𝐧𝐚𝐭𝐚•✠ • •══╩╝
+» 🥵𝙾𝚗𝚕𝚢𝚏𝚊𝚗𝚜 𝚎 𝚑𝚎𝚗𝚝𝚊𝚒 𝚗𝚘 𝚖𝚎𝚐𝚊🥵 «
+
+❁❧ *FOTOS DE HENTAI 720MB:* 
+_*🌐 https://suaurl.com/55eb2f 🌐*_
+
+❁❧ *PRINCESS STARLING(ONLY FANS)8.36GB:* 
+_*🌐 https://suaurl.com/b995e2 🌐*_
+
+❁❧ *ELLES CLUB 28.68GB:* 
+_*🌐 https://suaurl.com/8c135b 🌐*_
+
+❁❧ *COCONUTKITTY(ONLY FANS)7.69GB:*
+_*🌐 https://suaurl.com/5e91e0 🌐*_
+
+❁❧ *Belle delphine:* 
+_*🌐 https://suaurl.com/5c699d 🌐*_
+
+❁❧ *Love Lilah:* 
+_*🌐 https://suaurl.com/dbaae7 🌐*_
+
+❁❧ *Hidori:* 
+_*🌐 https://suaurl.com/f43f76 🌐*_`
+}
+exports.pack = pack
---
+++ src/playlist.js
@@ -0,0 +1,72 @@
+const playlist = (p) => {
+    return `╔╦══• •✠•𝐇𝐢𝐧𝐚𝐭𝐚•✠ • •══╦╗
+║
+║ 𝐁𝐎𝐓 : 「 𝐇𝐢𝐧𝐚𝐭𝐚 」
+║
+║ 𝐏𝐑𝐄𝐅𝐈𝐗𝐎 : /
+║
+║ 𝐃𝐎𝐍𝐎 : wa.me/+5575998868032
+║
+║𝘿𝘼𝙏𝘼 𝙀 𝙃𝙊𝙍𝘼 : ${date} 
+║
+║𝙐𝙎𝙐𝘼𝙍𝙄𝙊 : ${user} 
+║
+║𝙒𝘼𝙈𝙀 : ${wame} 
+║
+╚╩══• •✠•𝐇𝐢𝐧𝐚𝐭𝐚•✠ • •══╩╝
+ ╟➤m4*
+M4
+ ╟➤paypal*
+PayPal 💰
+ ╟➤mood*
+24kGoldn - Mood (Lyrics) ft. Iann Dior
+ ╟➤bebelean*
+LEAN - Superiority x Towy x Osquel x Beltito x Sammy x Falsetto
+ ╟➤raputin*
+Nightcore - Rasputin (Remix) (For Mother Russia)
+ ╟➤moskau*
+Nightcore - Moskau
+ ╟➤kalinka*
+Nightcore - Kalinka [Калинка]
+ ╟➤katyusha*
+[HARDBASS] Katyusha (Cosmowave Remix)
+ ╟➤yamete*
+Yamete kudasai
+ ╟➤tuebaiano*
+777-666 (Baiano Edition) - Lil Jappz (@caiojapa71)
+ ╟➤pneublind*
+CHEIRO DE BLINDING LIGHTS | LEOD
+ ╟➤cunouno*
+VOU COMER SEU C* NO UNO - CLIPE OFICIAL - MC Renanzin (Clipe Oficial)
+ ╟➤polishcow*
+POLISH COW
+ ╟➤urss*
+HINO DA U.R.S.S
+ ╟➤amgdamuie*
+Amiga Da Minha Mulher - Seu Jorge
+ ╟➤bcupernbam*
+NEGO BAM - BUTTERCUP | LEOD
+ ╟➤dstartnowpneu*
+CHEIRO DE DON'T START NOW OU CHEIRO DE TAMBORZINHO | LEOD
+ ╟➤wthisdown*
+SoulChef - Write This Down (Feat. Nieve)
+ ╟➤hentaigirl*
+MTC-S3RL
+ ╟➤hentaigirl2*
+S3RL-MTC2
+ ╟➤diekpop*
+Rapxis - Kpooper me Cancelou (Prod. H'erick)
+ ╟➤ilovedarla*
+Rapxis & Gohann - i Love Darla (prod. Astrobeattz)
+ ╟➤autistar*
+Rapxis - Autistar ( Prod. pdr0sa )
+ ╟➤shitpostgirl*
+Rapxis - Mina Shitposter (feat. Gohann)
+ ╟➤ftuberculose*
+Rapxis & Gohann - Flow Tuberculose (prod. YungDrum)
+ ╟➤goticrabuda*
+Rapxis - Gótica Rabuda (Feat. Libiuz)
+ ╟➤yougirlistenme*
+Rapxis - Sua Mina Me Escuta 🎧 (Prod. Gohann)`
+}
+exports.playlist = playlist
