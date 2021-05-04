1f228ec3bf2245d249ad3bbde2b1dec187e201d5
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-05-04 11:46:03 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,405 @@
+const help = (p, date, user, wame) => {
+return `╔╦══• •✠•𝐇𝐢𝐧𝐚𝐭𝐚•✠ • •══╦╗
+║
+║ 𝐁𝐎𝐓 : 「 𝐇𝐢𝐧𝐚𝐭𝐚」
+║
+║ 𝐏𝐑𝐄𝐅𝐈𝐗𝐎 : /
+║
+║ 𝐃𝐎𝐍𝐎 : wa.me/+5575998868032
+║
+║𝘿𝘼𝙏𝘼 𝙀 𝙃𝙊𝙍𝘼 : ${date} 
+║
+║𝙐𝙎𝙐𝘼𝙍𝙄𝙊 : ${user} 
+║
+║𝙒𝘼𝙈𝙀 : ${wame} 
+║
+╚╩══• •✠•𝐇𝐢𝐧𝐚𝐭𝐚•✠ • •══╩╝
+
+┏   「 Menu Principal 」
+╟➤listblockcmd 
+╟➤listpalavra 
+╟➤info 
+╟➤ajuda 
+╟➤help 
+╟➤comandos 
+╟➤menulite 
+╟➤menu 
+╟➤ping 
+╟➤destrava
+╟➤botvip 
+╟➤blocklist 
+╟➤blacklist 
+╟➤criador 
+╟➤toimg ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╽┷╼❥ 
+
+
+┏   「 Menu De Figurinhas 」
+╟➤stiker ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤sticker ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤fstiker ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤fsticker ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤nstiker ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nsticker ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nfstiker ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nfsticker ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nstickerv2 ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤attp ❮ᵗˣᵗ❯
+╟➤macaco 
+╽┷╼❥ 
+
+
+┏   「 Menu Editar Videos 」
+╟➤bold ❮ᵗˣᵗ❯
+╟➤poly ❮ᵗˣᵗ❯
+╟➤glowing ❮ᵗˣᵗ❯
+╟➤colorful ❮ᵗˣᵗ❯
+╟➤army ❮ᵗˣᵗ❯
+╟➤retro ❮ᵗˣᵗ❯
+╟➤frame ❮ˢᵉᶜ❯ ❮ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤mute ❮ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤rapidovid ❮ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤lentovid ❮ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤togif ❮ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤resizevid ❮ˡᵃᵍ❯ ❮ᵃˡᵗ❯ ❮ʳᵉᵖˡʸ ᵛⁱᵈ❯
+╽┷╼❥ 
+
+
+┏    「 Menu Logos 」
+╟➤missionpassed ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤nulis ❮ᵗˣᵗ❯
+╟➤watercolor ❮ᵗˣᵗ❯
+╟➤neonlogo ❮ᵗˣᵗ❯
+╟➤flowerlogo ❮ᵗˣᵗ❯
+╟➤matrixlogo ❮ᵗˣᵗ❯
+╟➤thunderlogo ❮ᵗˣᵗ❯
+╟➤silverplaca ❮ᵗˣᵗ❯
+╟➤goldplaca ❮ᵗˣᵗ❯
+╟➤narutologo ❮ᵗˣᵗ❯
+╟➤phlogo ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤mineconquista ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤shadow ❮ᵗˣᵗ❯
+╟➤glitch ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤ravetxt ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤woodtxt ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤neon ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤sunset ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤gimage ❮ᵗˣᵗ❯
+╟➤pglass ❮ᵗˣᵗ❯
+╟➤neonligth ❮ᵗˣᵗ❯
+╟➤coffe ❮ᵗˣᵗ❯
+╟➤galaxywp ❮ᵗˣᵗ❯
+╟➤whatis ❮ᵗˣᵗ❯
+╟➤txt3d ❮ᵗˣᵗ❯
+╽┷╼❥ 
+
+
+┏   「Menu Editar Fotos」
+╟➤backremove ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤cropimg ❮ˡᵃᵍ❯ ❮ᵃˡᵗ❯ ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤bwimg ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤emoji ❮ᵉᵐᵒʲⁱ❯
+╟➤beachnigth ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤checkporn ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤facedetect ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤identifyimg ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤monochromeimg ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤resizeimg ❮ˡᵃᵍ❯ ❮ᵃˡᵗ❯ ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤blurimg ❮ʳᵃᵈ❯ ❮ˢⁱᵍ❯ ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╽┷╼❥ 
+
+
+┏    「 Menu Pesquisa 」 
+╟➤fortniteshop 
+╟➤imagesearch ❮ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤map ❮ⁿᵃᵐᵉ❯
+╟➤brainlysrc ❮ⁿᵃᵐᵉ❯
+╟➤search ❮ⁿᵃᵐᵉ❯
+╟➤ssweb ❮ᵘʳˡ❯
+╟➤mediafire ❮ᵘʳˡ❯
+╟➤mob 
+╟➤minecraft 
+╟➤covidbrasil 
+╟➤covidmundo 
+╟➤foxnews 
+╟➤lofi 
+╟➤boy 
+╟➤girl 
+╟➤egirl 
+╟➤eboy 
+╟➤pinterest ❮ⁿᵃᵐᵉ❯
+╟➤wallpaper 
+╟➤fuckmylife 
+╟➤waifu 
+╟➤belle 
+╟➤saycat 
+╟➤biblia 
+╟➤animesrc ❮ⁿᵃᵐᵉ ᵃⁿⁱᵐᵉ❯
+╟➤mangasrc ❮ⁿᵃᵐᵉ ᵃⁿⁱᵐᵉ❯
+╟➤playstore ❮ⁿᵃᵐᵉ ᵃᵖᵖ❯
+╟➤happymod ❮ⁿᵃᵐᵉ ᵃᵖᵖ❯
+╟➤pokemon 
+╟➤ip ❮ip❯
+╟➤signome ❮ⁿᵃᵐᵉ❯
+╟➤gimage ❮ⁿᵃᵐᵉ❯
+╽┷╼❥ 
+
+
+┏    「 Menu Contagem  」
+╟➤halloween ❮ᵃⁿᵒ❯
+╟➤kidday ❮ᵃⁿᵒ❯
+╟➤motherday ❮ᵃⁿᵒ❯
+╟➤fatherday ❮ᵃⁿᵒ❯
+╟➤carnaval ❮ᵃⁿᵒ❯
+╟➤republic ❮ᵃⁿᵒ❯
+╟➤goodfriday ❮ᵃⁿᵒ❯
+╟➤finados ❮ᵃⁿᵒ❯
+╟➤ourlady ❮ᵃⁿᵒ❯
+╟➤independence ❮ᵃⁿᵒ❯
+╟➤workday ❮ᵃⁿᵒ❯
+╟➤tiradentes ❮ᵃⁿᵒ❯
+╟➤pascoa ❮ᵃⁿᵒ❯
+╟➤natal ❮ᵃⁿᵒ❯
+╟➤anonovo 
+╽┷╼❥ 
+
+
+┏    「 Menu Porno  」
+╟➤packmega18 
+╟➤nhentai ❮code❯
+╟➤randomhentai 
+╟➤gangbang 
+╟➤nsfwblowjob 
+╟➤panties 
+╟➤wallhentai 
+╟➤tentacles 
+╟➤nsfwneko 
+╟➤nsfwtrap 
+╟➤nsfw ❮1 ᵒʳ 0❯
+╟➤yaoi 
+╟➤ahegao 
+╟➤ecchi 
+╟➤cum 
+╟➤anal 
+╟➤pussy 
+╟➤ero 
+╟➤ahegao 
+╟➤yuri 
+╟➤hentaigif 
+╟➤hentainekogif 
+╟➤futanari 
+╽┷╼❥ 
+
+
+┏    「 Menu Modifica Áudio  」
+╟➤esquilo ❮ʳᵉᵖˡʸ ᵃᵘᵈⁱᵒ❯
+╟➤grave ❮ʳᵉᵖˡʸ ᵃᵘᵈⁱᵒ❯
+╟➤estourar ❮ʳᵉᵖˡʸ ᵃᵘᵈⁱᵒ❯
+╟➤rapidoaudio ❮ʳᵉᵖˡʸ ᵃᵘᵈⁱᵒ❯
+╟➤lentoaudio ❮ʳᵉᵖˡʸ ᵃᵘᵈⁱᵒ❯
+╟➤bass ❮ʳᵉᵖˡʸ ᵃᵘᵈⁱᵒ❯
+╽┷╼❥ 
+
+
+┏    「 Menu Baixa Músicas 」
+╟➤gtts ❮ˡᵃ❯ ❮ᵗˣᵗ❯
+╟➤playlist 
+╟➤lyrics 
+╟➤ptlyrics 
+╟➤play ❮ᵐᵘˢⁱᶜ ⁿᵃᵐᵉ❯
+╟➤playv2 ❮ᵐᵘˢⁱᶜ ⁿᵃᵐᵉ❯
+╟➤tomp3 ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤ytsearch ❮ⁿᵃᵐᵉ❯
+╟➤ytmp4 ❮ʸᵗ ᵘʳˡ❯
+╟➤ytmp3 ❮ʸᵗ ᵘʳˡ❯
+╽┷╼❥ 
+
+
+┏    「 Menu Gerador 」
+╟➤gpessoa 
+╟➤gendereco 
+╟➤gcnh 
+╟➤geleitortitulo 
+╟➤gbanco 
+╟➤gcarro 
+╟➤gpass ❮ˡᵉᵗᵗᵉʳ ᵠⁿᵗ❯
+╟➤cpf 
+╟➤ddd ❮ᵈᵈᵈ❯
+╟➤cep ❮ᶜᵉᵖ❯
+╟➤gerarcc 
+╟➤gprocesso 
+╽┷╼❥ 
+
+
+┏    「 Menu De Texto 」
+╟➤nameninja ❮ⁿᵃᵐᵉ❯
+╟➤fakeidentity 
+╟➤traduz ❮ˡᵃ❯ ❮ᵗˣᵗ❯
+╟➤wame 
+╟➤repeat ❮ᵗˣᵗ❯
+╟➤ocr ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤frase 
+╟➤wppim 
+╟➤db 
+╽┷╼❥ 
+
+
+┏    「 Menu Anime 」
+╟➤randomanime 
+╟➤randomshota 
+╟➤randomkiss 
+╟➤randomcry 
+╟➤randomhug 
+╟➤nekoanime 
+╟➤wait ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤slap 
+╟➤smile 
+╟➤piscada 
+╟➤dance 
+╟➤cringe 
+╟➤blush 
+╟➤gore 
+╟➤acenar 
+╟➤lick 
+╟➤bully 
+╟➤kemonomimi 
+╽┷╼❥ 
+
+
+┏    「 Menu De Rankings  」
+╟➤ranklindo 
+╟➤rankfeio 
+╟➤rankgado 
+╟➤rankcomu 
+╟➤ranknazi 
+╟➤rankqi 
+╟➤rankfofo 
+╟➤rankotaku 
+╟➤rankgay 
+╽┷╼❥ 
+
+
+┏    「 Menu De Previsões  」
+╟➤cassino 
+╟➤ship ❮ⁿᵘᵐ1❯ ❮ⁿᵘᵐ2❯
+╟➤corno ❮ⁿᵘᵐ1❯
+╟➤nazista ❮ⁿᵘᵐ1❯
+╟➤comunista ❮ⁿᵘᵐ1❯
+╟➤dado 
+╟➤gay ❮ⁿᵘᵐ1❯
+╟➤qi ❮ⁿᵘᵐ1❯
+╟➤seudia ❮ⁿᵘᵐ1❯
+╟➤feio ❮ⁿᵘᵐ1❯
+╟➤bv ❮ⁿᵘᵐ1❯
+╟➤gado ❮ⁿᵘᵐ1❯
+╟➤gostoso ❮ⁿᵘᵐ1❯
+╟➤gostosa ❮ⁿᵘᵐ1❯
+╟➤randomship ❮ⁿᵘᵐ1 ᵒʳ n/a❯
+╽┷╼❥ 
+
+
+┏    「 Menu Qr Code  」
+╟➤qrcode ❮ᵗˣᵗ❯
+╟➤qrcodebg ❮ᵗˣᵗ❯
+╟➤qrcodegb ❮ᵗˣᵗ❯
+╟➤qrcodebb ❮ᵗˣᵗ❯
+╟➤qrcodebb1 ❮ᵗˣᵗ❯
+╟➤qrcoderb ❮ᵗˣᵗ❯
+╟➤qrcodebr ❮ᵗˣᵗ❯
+╽┷╼❥ 
+
+
+┏    「 Menu Antis 」
+╟➤antidoc ❮1 ᵒʳ 0❯
+╟➤antiloc ❮1 ᵒʳ 0❯
+╟➤antiimg ❮1 ᵒʳ 0❯
+╟➤antipalavra ❮1 ᵒʳ 0❯
+╟➤antivideo ❮1 ᵒʳ 0❯
+╟➤antisticker ❮1 ᵒʳ 0❯
+╟➤antiaudio ❮1 ᵒʳ 0❯
+╟➤antictt ❮1 ᵒʳ 0❯
+╟➤antilink ❮1 ᵒʳ 0❯
+╟➤antiporn ❮1 ᵒʳ 0❯
+╟➤antifake ❮1 ᵒʳ 0❯
+╟➤antilinkhard ❮1 ᵒʳ 0❯
+╽┷╼❥ 
+
+
+┏    「 Menu De Grupos 」
+╟➤fechargp 
+╟➤mudardesc 
+╟➤mudarnome 
+╟➤abrirgp 
+╟➤linkgroup 
+╟➤listadmin 
+╟➤listonline 
+╟➤leave 
+╟➤kick ❮ᵈⁱᵃˡ ⁿᵘᵐ❯
+╟➤ban ❮ʳᵉᵖˡʸ ᵐˢᵍ❯
+╟➤promote ❮ᵈⁱᵃˡ ⁿᵘᵐ❯
+╟➤demote ❮ᵈⁱᵃˡ ⁿᵘᵐ❯
+╟➤add ❮ⁿᵘᵐ❯
+╟➤roletrussablock ❮1 ᵒʳ 0❯
+╟➤welcome ❮1 ᵒʳ 0❯
+╟➤simih ❮1 ᵒʳ 0❯
+╟➤autostickerimg ❮1 ᵒʳ 0❯
+╟➤onmodgrupo 
+╟➤offmodgrupo 
+╟➤autoreply ❮1 ᵒʳ 0❯
+╟➤marcar 
+╟➤marcar2 
+╟➤marcar3 
+╟➤hidemarcar ❮ᵗˣᵗ❯
+╽┷╼❥ 
+
+
+┏    「 Menu Stalkear 」
+╟➤instastalk ❮ⁿᵃᵐᵉ❯
+╟➤tiktokstalk ❮ⁿᵃᵐᵉ❯
+╟➤twitterstalk ❮ⁿᵃᵐᵉ❯
+╟➤ffstalk ❮ⁱᵈ ᶠʳᵉᵉ ᶠⁱʳᵉ❯
+╟➤ytstalk ❮ⁿᵃᵐᵉ❯
+╽┷╼❥ 
+
+
+┏    「 Menu De Zoação 」
+╟➤vsf 
+╟➤botfofo 
+╟➤pedro 
+╟➤botgostoso 
+╟➤botviado 
+╟➤botbaianor 
+╟➤botfdp 
+╟➤botfeio 
+╟➤botputa 
+╟➤botgay 
+╟➤botcorno 
+╟➤vtmnc 
+╟➤bomdia 
+╟➤boatarde 
+╟➤boanoite 
+╟➤lindo 
+╟➤cheguei 
+╽┷╼❥ 
+
+
+┏    「 Menu Só Do 𝑲𝑰𝑹𝑨 𝑾𝑨𝑹 」
+╟➤blockcmd ❮ᶜᵐᵈ ˡᵉˢˢ ᵖʳᵉᶠⁱˣ❯
+╟➤unblockcmd ❮ᶜᵐᵈ ˡᵉˢˢ ᵖʳᵉᶠⁱˣ❯
+╟➤alerta ❮ᵗˣᵗ❯
+╟➤infinityblock ❮1 ᵒʳ 0❯
+╟➤antiligar ❮1 ᵒʳ 0❯
+╟➤addpalavra ❮ᵗˣᵗ❯
+╟➤removepalavra ❮ᵗˣᵗ❯
+╟➤limparchat ❮ᵗˣᵗ❯
+╟➤ataque ❮ᵗˣᵗ❯
+╟➤divulgar ❮ᵗˣᵗ❯
+╟➤entrargp ❮ᵗˣᵗ❯
+╟➤block ❮ ⁿᵘᵐ❯
+╟➤unblock ❮ ⁿᵘᵐ❯
+╽┷╼❥`
+}
+
+exports.help = help
+
+
